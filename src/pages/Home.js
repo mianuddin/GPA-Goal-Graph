160d90eb73fab034a45de187f78ecb40b1789eea
Mian Uddin <mianuddin@gmail.com>
2016-04-25 13:14:28 -0700

    Made home a stateless functional component and replaced placeholder text with lorem ipsum.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,27 +5,23 @@ import HomeFormContainer from '../components/HomeFormContainer';
 import Paper from 'material-ui/lib/paper';
 import '../styles/partials/_Home';
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <div className="row">
-          <div className="col-xs-12">
-            <Paper zDepth={1} className="PaperContainer">
-              <h2>Plan and visualize your GPA goals</h2>
-              <p>I know words. I have the best words. He’s not a word hero. He’s a
-               word hero because he was captured. I like text that wasn’t captured.
-               Lorem Ipsum is unattractive, both inside and out. I fully understand
-               why it’s former users left it for something else. They made a good
-               decision. You know, it really doesn’t matter what you write as long
-               as you’ve got a young, and beautiful, piece of text.</p>
-            </Paper>
-          </div>
-        </div>
-        <HomeFormContainer />
+const Home = () => (
+  <div>
+    <div className="row">
+      <div className="col-xs-12">
+        <Paper zDepth={1} className="PaperContainer">
+          <h2>Plan and visualize your GPA goals</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+           Vivamus et vehicula justo, id elementum est. Suspendisse aliquet luctus iaculis.
+           Pellentesque sem felis, tempus tincidunt malesuada id, dignissim id urna.
+           Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+           Integer gravida egestas lacus, a suscipit elit condimentum eget.
+           In hac habitasse platea dictumst. Sed neque mi, tempor quis enim id.</p>
+        </Paper>
       </div>
-    );
-  }
-}
+    </div>
+    <HomeFormContainer />
+  </div>
+);
 
 export default Home;
