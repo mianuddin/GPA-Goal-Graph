31002f299fb660118012de3a9fb1a19449e1b443
Mian Uddin <mianuddin@gmail.com>
2016-05-01 17:37:54 -0700

    Updated initial state.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Router, useRouterHistory } from 'react-router';
 import { createHashHistory } from 'history';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { fromJS } from 'immutable';
 import reducer from './reducer';
 import routes from './routes';
 import injectTapEventPlugin from 'react-tap-event-plugin';
@@ -12,34 +13,32 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-const store = createStore(reducer,
-  window.devToolsExtension ? window.devToolsExtension() : undefined
-);
-
-// Set initial (test) state.
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    mainForm: {
-      includeClasses: false,
-      classTotal: {
-        grade: null,
-        credits: null,
-      },
-      canSubmit: true,
+const initialState = fromJS({
+  mainForm: {
+    includeClasses: false,
+    classTotal: {
+      grade: 0,
+      credits: 0,
     },
-    classes: [],
-    snackbar: {
-      open: false,
-      message: '',
-    },
-    classForm: {
-      selectedClass: 0,
-      canSubmit: true,
-      dialogOpen: false,
-    },
+    canSubmit: true,
+  },
+  classes: [],
+  snackbar: {
+    open: false,
+    message: '',
+  },
+  classForm: {
+    selectedClass: 0,
+    canSubmit: true,
+    dialogOpen: false,
   },
 });
+
+const store = createStore(
+  reducer,
+  initialState,
+  window.devToolsExtension ? window.devToolsExtension() : undefined
+);
 
 const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 
