5854bcf8b8b0a45e73d5a22431e20970b9723a69
Mian Uddin <mianuddin@gmail.com>
2016-06-25 14:53:08 -0700

    Added image banner to homepage.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,22 +2,28 @@ import React from 'react';
 
 import HomeFormContainer from '../components/HomeFormContainer';
 
-import Paper from 'material-ui/lib/paper';
+import { Card, CardMedia, CardTitle, CardText } from 'material-ui';
 import '../styles/partials/_Home';
 
 const Home = () => (
   <div>
     <div className="row">
       <div className="col-xs-12">
-        <Paper zDepth={1} className="PaperContainer">
-          <h2>Plan and visualize your GPA goals</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-           Vivamus et vehicula justo, id elementum est. Suspendisse aliquet luctus iaculis.
-           Pellentesque sem felis, tempus tincidunt malesuada id, dignissim id urna.
-           Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-           Integer gravida egestas lacus, a suscipit elit condimentum eget.
-           In hac habitasse platea dictumst. Sed neque mi, tempor quis enim id.</p>
-        </Paper>
+        <Card>
+          <CardMedia
+            overlay={<CardTitle title="Plan and achieve your GPA goals." />}
+          >
+            <img src="http://i.imgur.com/xUsQZXR.jpg" />
+          </CardMedia>
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Vivamus et vehicula justo, id elementum est. Suspendisse aliquet luctus iaculis.
+            Pellentesque sem felis, tempus tincidunt malesuada id, dignissim id urna.
+            Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Integer gravida egestas lacus, a suscipit elit condimentum eget.
+            In hac habitasse platea dictumst. Sed neque mi, tempor quis enim id.
+          </CardText>
+        </Card>
       </div>
     </div>
     <HomeFormContainer />
